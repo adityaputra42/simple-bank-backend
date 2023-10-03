e728f218fb9162ee6e46270085b524e9080ec378
adityaputra42 <aditiyaputra42@gmail.com>
2023-10-03 11:36:07 +0700

    add store db transaction

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,80 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferTx(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := CreateRandomAccount(t)
+	account2 := CreateRandomAccount(t)
+
+	// run concurent transfer transaction
+	n := 5
+	amount := int64(10)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
+				Amount:        amount,
+			})
+			errs <- err
+			results <- result
+		}()
+
+	}
+
+	// check result
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+		result := <-results
+		require.NotEmpty(t, result)
+
+		// check Transfer
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		// check From Entry
+		fromEntries := result.FromEntry
+		require.NotEmpty(t, fromEntries)
+		require.Equal(t, account1.ID, fromEntries.AccountID)
+		require.Equal(t, -amount, fromEntries.Amount)
+		require.NotZero(t, fromEntries.ID)
+		require.NotZero(t, fromEntries.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntries.ID)
+		require.NoError(t, err)
+
+		// check To Entry
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+		require.Equal(t, account2.ID, toEntry.AccountID)
+		require.Equal(t, amount, toEntry.Amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+
+		// check account balance
+	}
+
+}
