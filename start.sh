365484c163a8c718aab91719d59025a490b64444
adityaputra42 <aditiyaputra42@gmail.com>
2024-12-29 23:39:12 +0700

    update docker compose

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "run db migration"
-/app/migrate -path /app/migration -database "$DB_SOURCE" -versbose up
+/app/migrate -database "$DB_SOURCE" -path /app/migration -verbose up
 
 echo "start the app"
 exec "$@"
