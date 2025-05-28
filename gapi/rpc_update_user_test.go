2444a6f66a5643c84a7198d7df214397daeabf17
adityaputra42 <aditiyaputra42@gmail.com>
2025-05-28 11:36:57 +0700

    add unittest with authorization in grpc

---
+++ gapi/rpc_update_user_test.go
@@ -0,0 +1,99 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+	mockdb "simple-bank/db/mock"
+	db "simple-bank/db/sqlc"
+	"simple-bank/pb"
+	"simple-bank/token"
+	"simple-bank/util"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUpdateUserApi(t *testing.T) {
+	user, _ := randomUser(t)
+
+	newEmail := util.RandomEmail()
+	newName := util.RandomOwner()
+
+	testCases := []struct {
+		name          string
+		body          *pb.UpdateUserRequest
+		buildStubs    func(store *mockdb.MockStore)
+		buildContext  func(t *testing.T, tokenMaker token.Maker) context.Context
+		checkResponse func(t *testing.T, res *pb.UpdateUserResponse, err error)
+	}{
+		{
+			name: "Ok",
+			body: &pb.UpdateUserRequest{
+				Username: user.Username,
+				FullName: &newName,
+				Email:    &newEmail,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.UpdateUserParams{
+					Username: user.Username,
+
+					Email: sql.NullString{
+						String: newEmail,
+						Valid:  true,
+					},
+					FullName: sql.NullString{
+						String: newName,
+						Valid:  true,
+					},
+				}
+				updatedUser := db.User{
+					Username:          user.Username,
+					HashedPassword:    user.HashedPassword,
+					FullName:          newName,
+					Email:             newEmail,
+					PasswordChangedAt: user.PasswordChangedAt,
+					CreatedAt:         user.CreatedAt,
+					IsVerifiedEmail:   user.IsVerifiedEmail,
+				}
+				store.EXPECT().UpdateUser(gomock.Any(), gomock.Eq(arg)).Times(1).Return(updatedUser, nil)
+
+			},
+			buildContext: func(t *testing.T, tokenMaker token.Maker) context.Context {
+				ctx := newContextWithBearerToken(t, tokenMaker, user.Username, time.Minute)
+				return ctx
+			},
+			checkResponse: func(t *testing.T, res *pb.UpdateUserResponse, err error) {
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				updatedUser := res.GetUser()
+				require.Equal(t, user.Username, updatedUser.Username)
+				require.Equal(t, newName, updatedUser.FullName)
+				require.Equal(t, newEmail, updatedUser.Email)
+			},
+		},
+	}
+
+	for i := range testCases {
+
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+
+			storeCtrl := gomock.NewController(t)
+			defer storeCtrl.Finish()
+			store := mockdb.NewMockStore(storeCtrl)
+
+			tc.buildStubs(store)
+			server := NewTestServer(t, store, nil)
+
+			ctx := tc.buildContext(t, server.tokenMaker)
+			res, err := server.UpdateUser(ctx, tc.body)
+
+			tc.checkResponse(t, res, err)
+		})
+
+	}
+
+}
